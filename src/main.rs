272a4370d3910f563311688598162042ac934c9b
codeslayer <willyanpublic@protonmail.com>
2023-04-16 17:42:38 -0300

    add output file

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,23 @@ use events_cli::{
     event::{Attendee, EventData},
     sql::ToSQL,
 };
-use std::path::PathBuf;
+use std::{io::Write, path::PathBuf};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     /// Event Data CSV file
-    #[arg(short, long, value_parser = is_valid_file)]
+    #[arg(short, long, value_parser = input_file)]
     event: PathBuf,
     /// Attendees Info CSV file
-    #[arg(short, long = "atts", value_parser = is_valid_file)]
+    #[arg(short, long = "atts", value_parser = input_file)]
     attendees: PathBuf,
+    /// SQL queries output file
+    #[arg(short, long)]
+    output: PathBuf,
 }
 
-fn is_valid_file(s: &str) -> Result<PathBuf, String> {
+fn input_file(s: &str) -> Result<PathBuf, String> {
     let path = std::path::Path::new(s);
     if !path.exists() {
         return Err("file does not exist".to_owned());
@@ -63,7 +66,10 @@ fn main() {
     let atts = attendees(buffer);
 
     let evt = evt.as_event(atts);
+    let queries = evt.to_sql().into_queries();
 
-    println!("{:?}\n", evt);
-    println!("{}", evt.to_sql().into_queries());
+    std::fs::File::create(args.output)
+        .expect("failed to create output file")
+        .write_all(queries.as_bytes())
+        .expect("failed to write into output file");
 }
