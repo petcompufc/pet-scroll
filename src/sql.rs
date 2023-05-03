88ff91ccc4fe16a4e6a8ca6d682e7beacbc515f7
codeslayer <willyanpublic@protonmail.com>
2023-05-03 15:15:36 -0300

    add basic tests to slq module

--- src/sql.rs
+++ src/sql.rs
@@ -2,7 +2,7 @@ pub trait ToSQL {
     fn to_sql(&self) -> QueryPool;
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone, PartialEq)]
 pub struct QueryPool {
     pool: Vec<String>,
 }
@@ -79,5 +79,75 @@ impl Request {
 impl std::fmt::Display for Request {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "USE {};\n{}", self.db, self.queries)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_pool() {
+        let queries = vec!["QUERY 1".to_owned(), "QUERY 2".to_owned()];
+        let pool = QueryPool::new().queries(queries.clone());
+        assert_eq!(queries, pool.into_iter().collect::<Vec<_>>());
+    }
+
+    #[test]
+    fn pool_add() {
+        let mut pool = QueryPool::new();
+        pool.add("QUERY 1".to_owned());
+        pool.add("QUERY 2".to_owned());
+        assert_eq!(
+            vec!["QUERY 1", "QUERY 2"],
+            pool.into_iter().collect::<Vec<_>>()
+        );
+    }
+
+    #[test]
+    fn pool_add_many() {
+        let queries = vec!["QUERY 1".to_owned(), "QUERY 2".to_owned()];
+        let mut pool = QueryPool::new();
+        pool.add_many(queries.clone());
+        assert_eq!(queries, pool.into_iter().collect::<Vec<_>>());
+    }
+
+    #[test]
+    fn pool_iter() {
+        let queries = vec!["QUERY 1".to_owned(), "QUERY 2".to_owned()];
+        let mut pool = QueryPool::new();
+        pool.add_many(queries.clone());
+        let result: Vec<_> = queries.clone().into_iter().collect();
+        assert_eq!(queries, result);
+    }
+
+    #[test]
+    fn pool_to_req() {
+        let queries = vec!["QUERY 1".to_owned(), "QUERY 2".to_owned()];
+        let pool = QueryPool::new().queries(queries);
+        let req = pool.clone().into_req("database");
+        assert_eq!(pool, req.queries);
+    }
+
+    #[test]
+    fn pool_display() {
+        let pool = QueryPool::new().queries(["QUERY 1".to_owned(), "QUERY 2".to_owned()]);
+        let str = "QUERY 1;\nQUERY 2";
+        assert_eq!(str, pool.to_string());
+    }
+
+    #[test]
+    fn create_req() {
+        let pool = QueryPool::new().queries(["QUERY 1".to_owned(), "QUERY 2".to_owned()]);
+        let req = Request::new("database").queries(pool.clone());
+        assert_eq!(pool, req.queries);
+    }
+
+    #[test]
+    fn req_display() {
+        let pool = QueryPool::new().queries(["QUERY 1".to_owned(), "QUERY 2".to_owned()]);
+        let req = Request::new("database").queries(pool);
+        let str = "USE database;\nQUERY 1;\nQUERY 2";
+        assert_eq!(str, req.to_string());
     }
 }
